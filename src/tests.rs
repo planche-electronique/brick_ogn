de0b8b1351883af06e1b99bc96af7e6d3a363392
Hugo Berthet-Rambaud <hugoberthetrambaud@outlook.com>
2024-01-26 21:40:13 +0100

    add a test for the misea jour obsoletes functions

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,8 @@
 #[cfg(test)]
-use {crate::planche::MiseAJour, crate::planche::Planche, json};
+use {
+    crate::planche::{mise_a_jour::MiseAJourObsoletes, MiseAJour, Planche},
+    json,
+};
 
 #[test]
 fn test_miseajour_parse() {
@@ -10,4 +13,13 @@ fn test_miseajour_parse() {
     let mut maj_mod = MiseAJour::new();
     maj_mod.parse(maj_def_json).unwrap();
     assert_eq!(maj_def, maj_mod)
+}
+
+#[test]
+fn t_enlever_maj_obsoletes() {
+    let mut vec_majs = vec![MiseAJour::new(), MiseAJour::new()];
+
+    vec_majs.enlever_majs_obsoletes(chrono::Duration::minutes(5));
+    let empty_vec: Vec<MiseAJour> = Vec::new();
+    assert_eq!(empty_vec, vec_majs)
 }
